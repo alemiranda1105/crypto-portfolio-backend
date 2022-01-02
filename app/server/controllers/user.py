02ac0ac262f03df247f3e9db030c2b9314c2d1b0
Alejandro Miranda <amiranda110500@gmail.com>
2022-01-02 11:33:27 +0000

    Sign up user

--- app/server/controllers/user.py
+++ app/server/controllers/user.py
@@ -4,9 +4,16 @@ from app.server.database import user_collection, user_helper
 
 
 async def retrieve_user(user_id: str) -> dict:
-    print(ObjectId(user_id))
-
     user = await user_collection.find_one({"_id": ObjectId(user_id)})
-    print(user)
     if user:
         return user_helper(user)
+
+
+async def sign_up(user_data):
+    email_exists = await user_collection.find_one({"email": user_data["email"]})
+    if email_exists:
+        return False
+    else:
+        user = await user_collection.insert_one(user_data)
+        new_user = await user_collection.find_one({"_id": user.inserted_id})
+        return user_helper(new_user)
--- app/server/routes/user.py
+++ app/server/routes/user.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
 
-from app.server.controllers.user import retrieve_user
+from app.server.controllers.user import retrieve_user, sign_up
 from app.server.models.response import ResponseModel
+from app.server.models.user import UserSchema
 
 router = APIRouter(
     prefix="/users",
@@ -13,3 +15,10 @@ router = APIRouter(
 async def get_user_by_id(user_id):
     users = await retrieve_user(user_id)
     return ResponseModel(users, "User with id {}".format(user_id))
+
+
+@router.post("/")
+async def add_user(user: UserSchema = Body(...)):
+    user = jsonable_encoder(user)
+    new_user = await sign_up(user)
+    return ResponseModel(new_user, "Registro completado")
